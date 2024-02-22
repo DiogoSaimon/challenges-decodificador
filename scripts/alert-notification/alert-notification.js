70a5888405d1c1a0bdebad92674f9e2e32aafa44
Diogo Saimon <diogosaimon@hotmail.com>
2024-02-22 13:24:51 -0300

    novas componentizações nos scripts e melhorias na responsividade padrão do projeto

---
+++ scripts/alert-notification/alert-notification.js
@@ -0,0 +1,44 @@
+"use strict";
+
+export function copyTextAlert() {
+    let alertNotification = document.querySelector(".alert");
+    let alertNotificationMessage = document.querySelector(".alert_message");
+
+    alertNotificationMessage.innerText = "Text copiado";
+
+    if (!alertNotification.hasAttribute("alert copy_hide")) {
+        alertNotification.className = "alert copy_hide";
+    }
+
+    alertNotification.classList.remove("copy_hide");
+    alertNotification.classList.add("copy_show");
+    alertNotification.classList.add("showCopyAlert");
+
+
+    setTimeout(() => {
+        alertNotification.classList.add("copy_hide");
+        alertNotification.classList.remove("copy_show");
+    }, 5000);
+};
+
+export function inputEmpty(functionName) {
+    let alertNotification = document.querySelector(".alert");
+    let alertNotificationMessage = document.querySelector(".alert_message");
+
+    alertNotificationMessage.innerText = `Por favor, digitar algo para prosseguir com a ${functionName}`;
+
+    if(!alertNotification.hasAttribute("alert hide")){
+        alertNotification.className="alert hide";
+    }
+
+    alertNotification.classList.remove("hide");
+    alertNotification.classList.add("show");
+    alertNotification.classList.add("showAlert");
+
+
+    setTimeout(() => {
+        alertNotification.classList.add("hide");
+        alertNotification.classList.remove("show");
+    }, 5000);
+
+ }
---
+++ scripts/get-text/get-text.js
@@ -0,0 +1,19 @@
+"use script";
+
+
+let inputTextField = document.querySelector("textarea");
+inputTextField.placeholder = "Digite seu texto";
+
+export let phrase = "";
+
+const form = document.forms.namedItem("encrypt-decrypt");
+form.addEventListener("input", () => {
+    const message = form.message.value;
+
+    const formData = new FormData(form);
+
+    formData.set("message", message);
+
+    phrase = formData.get("message");
+})
+
